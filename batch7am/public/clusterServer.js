c7eb56f644d71109f3c2a9b5a6f13d7de871e0ae
durgaprasadUI <62068712+durgaprasadUI@users.noreply.github.com>
2020-03-30 10:32:19 +0530

    Mar 30
    Mar 30

---
+++ batch7am/public/clusterServer.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var cluster = require("cluster");
+
+
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+	for (var i = 0; i < 4; i++) {
+		cluster.fork();
+		console.log("created server " + i)
+	}
+} else {
+	var server = http.createServer(function(req, res){
+		console.log("the process from which it got response is " + process.pid);
+		res.end("hello process is " + process.pid);
+	}); 
+
+
+	server.listen(8080, function(){
+		console.log("server is listing at 8080 with process id " + process.pid);
+	});
+}
+
