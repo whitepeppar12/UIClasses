42ef6b82ded22d4fef3a66def0668faac412d2b7
durgaprasadUI <62068712+durgaprasadUI@users.noreply.github.com>
2020-05-08 10:17:26 +0530

    May 8
    May 8

--- batch830am/app.js
+++ batch830am/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var prodRouter = require("./routes/productDetails");
+
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use("/get/data/productdetails", prodRouter);
 
 app.listen(8081, function(){
 	console.log("server is listing at 8081")
--- batch830am/public/May_8/DHTML/prodApp.js
+++ batch830am/public/May_8/DHTML/prodApp.js
@@ -31,15 +31,20 @@ function showProductDetails(productData) {
 }
 
 function getProductData(){
+	$(".loadingBlock").show();
 	$.ajax({
 		url: 'http://localhost:8081/get/data/productdetails',
 		method: 'GET',
 		dataType: 'JSON',
 		success: function(res) {
+			$(".loadingBlock").hide();
 			loadProductDetails(res.productDetails);
 		},
 		error: function(err) {
 			console.log("error");
+			//document.querySelector(".errBlock").style.display = 'block';
+			$(".loadingBlock").hide();
+			$(".errBlock").show();
 		}
 	})
 }
