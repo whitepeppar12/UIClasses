b276f7e6f1b17b9eb0108127a8366de6685647fb
durgaprasadUI <62068712+durgaprasadUI@users.noreply.github.com>
2020-06-20 10:20:00 +0530

    June_20
    June_20

---
+++ Batch7am-May2020-Server/public/June_20/assignment_15.js
@@ -0,0 +1,55 @@
+var countryData = [
+	{
+		id: "cntry_01",
+		name: 'India'
+	},
+	{
+		id: "cntry_02",
+		name: 'USA'
+	},
+	{
+		id: "cntry_03",
+		name: 'Russia'
+	}
+];
+
+
+var stateData = {
+	
+	"cntry_01": [
+		{
+			sId: 'state_123',
+			sname: 'Maharastra'
+		},
+		{
+			'sId': "state_124",
+			sname: 'AndhraPradesh'
+		},
+		{
+			'sId': 'state_125',
+			sname: 'Punjab'
+		}
+	],
+	"cntry_02": [
+		{
+			sId: 'state_201',
+			sname: 'Florida'
+		},
+		{
+			'sId': "state_202",
+			sname: 'Newyork City'
+		},
+		{
+			'sId': 'state_203',
+			sname: 'akjhfa'
+		}
+	]
+	...
+	..
+
+};
+
+var districtNames = {
+	"state_201": ["abc district", "pqr distic", ...],
+	"state_123": [asdfasdfasdfasfa...]
+}
