7c0e7a6c6bf97384a68e266c1b0122112c4b6265
rupali <rupali.l@moentek.com>
2020-06-22 11:01:25 +0530

    RL :

---
+++ Batch7am-May2020-Server/public/Practice/rupali/22-0-2020/app.js
@@ -0,0 +1,22 @@
+var sDetails = {};
+function getRegDetails(){
+    
+    sDetails.sname = document.querySelector("#sName").value;
+    sDetails.sage = document.querySelector("#sAge").value;
+    sDetails.country = document.querySelector("#sCountry").value;
+    sDetails.class = document.querySelector("#sClass").value;
+    
+    console.log(sDetails);
+    showStudentDetails();
+}
+
+function showStudentDetails(){
+    var ulList = document.createElement("ul");
+    for(var temp in sDetails){
+        var liTag = document.createElement("li");
+        liTag.innerHTML = temp + ":" + sDetails[temp];
+        ulList.append(liTag);
+    }
+    console.log(ulList);
+    document.querySelector("#displayBlock").append(ulList);
+}
