a11563980649cc5ab5d6334e55c882f950da4a51
[RupaliLande] <[Your>
2020-06-28 11:35:51 +0530

    userdefined exception practice

---
+++ Batch7am-May2020-Server/public/Practice/rupali/26-06-2020/app.js
@@ -0,0 +1,42 @@
+function registerDetails() {
+	var data = {};
+	data.name = document.querySelector("#uname").value;
+	data.age = document.querySelector("#userAge").value;
+
+	/*if (data.name == '') {
+		document.querySelector("#nameErr").style.display = 'block';
+	}
+	var msg;
+	if (data.age == '') {
+		msg = "Age is mandry field";
+		document.querySelector("#ageErr").innerHTML = msg;
+	} else if (data.age < 21) {
+			
+		msg = "U dnt have enough age validation to join";
+		document.querySelector("#ageErr").innerHTML = msg;
+	}
+	console.log(data);*/
+
+	try {
+		if (data.name == '') {
+			// its an exception...
+			throw "name error";
+		} 
+		if (data.age == '') {
+			throw 'age empty error';
+		} else if (data.age < 21) {
+			throw 'age smaller error';
+		}
+	} catch(error) {
+		if (error == 'name error') {
+			document.querySelector("#nameErr").style.display = 'block';
+		} else if (error == 'age empty error') {
+			msg = "Age is mandry field";
+			//document.querySelector("#ageErr").innerHTML = msg;
+			displaydialog(msg)
+		} else if (error == 'age smaller error') {
+			msg = "U dnt have enough age validation to join";
+			document.querySelector("#ageErr").innerHTML = msg;
+		}
+	}
+} 
