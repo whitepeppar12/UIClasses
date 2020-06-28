aaf67c392eafd61834d3ed69de58a437141ec9d8
[RupaliLande] <[Your>
2020-06-28 13:00:23 +0530

    practice -- problem without closure

---
+++ Batch7am-May2020-Server/public/Practice/rupali/27-06-2020/app.js
@@ -0,0 +1,26 @@
+var counter = 0;
+function dojob(type) {
+    counter++;
+    var fval = document.querySelector("#fval").value;
+    var sval = document.querySelector("#sval").value;
+    fval = parseInt(fval)
+    sval = parseInt(sval)
+    var result;
+    switch(type) {
+        case 'add':
+            result = doAddition(fval, sval);
+            break;
+        case 'sub':
+            result = doSubstraction(fval, sval);
+            break;
+        case 'div':
+            result = doDivision(fval, sval);
+            break;
+        case 'mul':
+            result = doMultiplication(fval, sval);
+            break;
+    }
+    var msg = "The operation " + type + ' is : ' + result;
+    msg += "   No. of times user used options is " + counter;
+    document.querySelector("#rblock").innerText = msg;
+} 
