4a49063343df348bdf35fe1734edf1aba3f05579
durgaprasadUI <62068712+durgaprasadUI@users.noreply.github.com>
2020-06-29 08:18:34 +0530

    JUNE 29
    JUNE 29

---
+++ Batch7am-May2020-Server/public/June_29/app.js
@@ -0,0 +1,30 @@
+var counter = 0;
+var mathUtil = "test";
+function addValues() {
+
+}
+function dojob(type) {
+    counter++;
+    var fval = document.querySelector("#fval").value;
+    var sval = document.querySelector("#sval").value;
+    fval = parseInt(fval)
+    sval = parseInt(sval)
+    var result;
+    switch(type) {
+        case 'add':
+            result = mathUtil.addValues(fval, sval)
+            break;
+        case 'sub':
+            result = doSubstraction(fval, sval)
+            break;
+        case 'div':
+            result = doDivision(fval, sval);
+            break;
+        case 'mul':
+            result = mathUtil.doMultiplication(fval, sval)
+            break;
+    }
+    var msg = "The operation " + type + ' is : ' + result;
+    msg += "   No. of times user used options is " + counter;
+    document.querySelector("#rblock").innerText = msg;
+}
---
+++ Batch7am-May2020-Server/public/June_29/lib/mathOperations.js
@@ -0,0 +1,41 @@
+
+var mathUtil = (function(){
+    var counter = 0;
+
+    function doAddition(firstValue, secondValue) {
+        counter++;
+        var result = 0;
+        result = firstValue + secondValue;
+        return result;
+    }
+
+    function doSubstraction(firstValue, secondValue) {
+        counter++;
+        var result = 0; 
+        result = firstValue - secondValue;
+        return result;
+    }
+
+    
+
+    function doDivision(firstValue, secondValue) {
+        counter++;
+        var result = 0; 
+        result = firstValue / secondValue;
+        return result;
+    }
+
+    return {
+       addValues: function(a, b) {
+            var res = doAddition(a, b);
+            return res;
+       },
+       doMultiplication: function(firstValue, secondValue) {
+            counter++;
+            var result = 0; 
+            result = firstValue * secondValue;
+            return result;
+    }
+    }
+})();
+
